508ae34baa9987922278480d2d690df03ebe8f10
Mudasir Ahanger <mudasirahanger@Mudasirs-MacBook-Pro.local>
2024-01-17 15:52:55 +0530

    api start for app

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/login', function (Request $request) {
+    //return $request->user();
+   // dd($request->name);
+    return response()->json([
+        'name' => $request->name,
+        'state' => 'CA',
+    ]);
+});
